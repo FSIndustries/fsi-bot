849bab0a2eb3e97a0572bf1f0bd529d41a8534c9
Lukas Rezek <lukas@miratronix.com>
2016-11-19 15:53:46 -0500

    Add local.json file overrides

--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@ const nconf = require('nconf');
 
 nconf.argv()
     .env({ lowerCase: true, separator: ':' })
+    .file('local', { file: `${__dirname}/../config/local.json` })
     .file('defaults', { file: `${__dirname}/../config/default.json` });
 
 module.exports = nconf;
