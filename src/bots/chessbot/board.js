e37b83c84b60399e9f5b85ced93a869fab711e8d
AmazingHorse <billheughan@gmail.com>
2016-11-20 14:19:28 -0500

    fixed erroneous map() call, now forown()

--- src/bots/chessbot/board.js
+++ src/bots/chessbot/board.js
@@ -4,23 +4,23 @@ const chess = require('chess.js');
 const black = '\u265A';
 const allbox = _.map(_.range(9472, 9472+200), b => String.fromCharCode(b));
 const box = _.forOwn({
-    vbar: 2,
-    hbar: 0,
-    ul  :12,
-    ur  :16,
-    ll  :20, 
-    lr  :24,
-    nt  :44,
-    st  :52,
-    wt  :28,
-    et  :36,
+    vbar: 3,
+    hbar: 1,
+    ul  :13,
+    ur  :17,
+    ll  :21,
+    lr  :25,
+    nt  :45,
+    st  :53,
+    wt  :29,
+    et  :37,
     plus:60
   },
 (val, key, obj) => {
   obj[key] = String.fromCharCode(val+9472);
 });
 
-const pieces = _.map({
+const pieces = _.forOwn({
   wKing: 0,
   wQueen: 1,
   wRook: 2,
@@ -36,7 +36,7 @@ const pieces = _.map({
 },
     (v, k, o) => o[k] = String.fromCharCode(v+9812));
 
-const h3 = `${box.hbar}`.repeat(3);
+const h3 = `${box.hbar}`.repeat(2);
 
 const innertop = `${h3}${box.nt}`.repeat(7);
 const innermid = `${h3}${box.plus}`.repeat(7);
@@ -46,14 +46,14 @@ const topline = `${box.ul}${innertop}${h3}${box.ur}\n`;
 const midline = `${box.wt}${innermid}${h3}${box.et}\n`;
 const botline = `${box.ll}${innermid}${h3}${box.lr}\n`;
 
-// output from fen parser goes here
-const tuple = box.vbar +  `    ${black}   ${box.vbar}`.repeat(8) + '\n';
+// output from fen/zobrist parser goes here
+const tuple = box.vbar +  `  ${black}  ${box.vbar}`.repeat(8) + '\n';
 
 const rowsBottom = `${tuple}${midline}`;
 
 module.exports = {
-    render: () =>{
-      const board = topline + `${tuple}${midline}`.repeat(6) + tuple + botline;
+    render: () => {
+      const board = topline + `${tuple}${midline}`.repeat(7) + tuple + botline;
       return board;
     }
 };
