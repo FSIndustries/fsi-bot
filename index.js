c47f99d9abf9e1ec4c0b8bb7b39dd88b2d2df139
AmazingHorse <billheughan@gmail.com>
2016-11-20 12:59:28 -0500

    Added chessbot 0.1: prints misshapen chessboard

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 'use strict';
 
-require('./src/botloader.js');
+require('./src');
---
+++ src/bots/chessbot/chess.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const _ = require('lodash');
+//const chess = new Chess();
+
+module.exports = {
+    new: (message, body) => {
+
+        return "Chess game created";
+    }
+};
