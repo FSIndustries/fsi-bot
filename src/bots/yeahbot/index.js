d728fdc8ea7bdd06e0debb1f2dd3e64c3334eb70
AmazingHorse <billheughan@gmail.com>
2016-11-25 02:02:42 -0500

    fixed codepoint references, escaped underscores

--- src/bots/yeahbot/index.js
+++ src/bots/yeahbot/index.js
@@ -1,25 +1,30 @@
 'use strict';
 
+// offset 0x10
 const arm =
-    require('unicode-8.0.0/Block/Box_Drawing/Symbols').get(0x10);
+    require('unicode-8.0.0/Block/Miscellaneous_Technical/Symbols')[16];
+// offset OxA0
 const glass =
-    require('unicode-8.0.0/Block/Geometric_Shapes/Symbols').get(0xA0);
+    require('unicode-8.0.0/Block/Geometric_Shapes/Symbols')[0];
+    // offset 0x22
 const eye =
-    require('unicode-8.0.0/Block/General_Punctuation/Symbols').get(0x22);
+    require('unicode-8.0.0/Block/General_Punctuation/Symbols')[34];
 
-const face = `${eye}\_${eye}`;
+const face = `${eye}\\_${eye}`;
 
 const glasses = `${arm}${glass}-${glass}`;
 
-const glassesFace = `${arm}${glass}\_${glass}`;
+const glassesFace = `${arm}${glass}\\_${glass}`;
 
-const yeeeeeaaaaahhhh = `(${face})\n( ${face})>${glasses}\n(${glassesFace}`;
+const yeeeeeaaaaahhhh = `(${face})\n( ${face})>${glasses}\n(${glassesFace})`;
 
 module.exports = {
-    onGlasses: () => {
-        message.reply(yeeeeeaaaaahhhh);
-    },
-    onCsi: () => {
-        message.reply(yeeeeeaaaaahhhh);
+    onCommand: {
+        glasses: (message) => {
+            message.reply(yeeeeeaaaaahhhh);
+        },
+        csi: (message) => {
+            message.reply(yeeeeeaaaaahhhh);
+        }
     }
 };
